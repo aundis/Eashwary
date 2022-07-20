a9321330ef6c1541c7fedddfdbb4e6787e73848c
Eashwary <eashwary.hm@sstsinc.com>
2022-07-20 13:03:31 +0530

    latest builds

---
+++ EXECUTABLES/JAYMIT/BUILDS/WebDriverBuild13.4/Build/Intermediates.noindex/WebDriverAgent.build/Debug-iphoneos/WebDriverAgentLib.build/StaticAnalyzer/WebDriverAgent/WebDriverAgentLib/normal/arm64/WebDriverAgentLib_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/eashwary/ios/WebDriverAgent-csdwmqqdvcsqyjhayfuncroevzub/Build/Intermediates.noindex/WebDriverAgent.build/Debug-iphoneos/WebDriverAgentLib.build/DerivedSources/WebDriverAgentLib_vers.c
