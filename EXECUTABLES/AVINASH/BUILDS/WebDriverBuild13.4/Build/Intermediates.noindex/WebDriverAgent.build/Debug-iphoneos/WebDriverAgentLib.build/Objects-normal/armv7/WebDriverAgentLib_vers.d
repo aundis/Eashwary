8b9d357ee4fd8036b6827e8e6ec76b0ad6678687
Eashwary <eashwary.hm@sstsinc.com>
2022-08-05 17:42:37 +0530

    safari

--- EXECUTABLES/AVINASH/BUILDS/WebDriverBuild13.4/Build/Intermediates.noindex/WebDriverAgent.build/Debug-iphoneos/WebDriverAgentLib.build/Objects-normal/armv7/WebDriverAgentLib_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/eashwary/ios/WebDriverAgent-csdwmqqdvcsqyjhayfuncroevzub/Build/Intermediates.noindex/WebDriverAgent.build/Debug-iphoneos/WebDriverAgentLib.build/DerivedSources/WebDriverAgentLib_vers.c
